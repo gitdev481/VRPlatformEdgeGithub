deaa13cb456e15a3982d58a9420157aa011b35a9
julian_stopher <julianstopher@outlook.com>
2018-12-18 00:56:16 +0000

    Train animation
    -Configured the female avatar to accept the idle animation.
    -Animated the train coming into the station and having all the doors open simultaneously.

---
+++ VR Platform Edge/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+
+    #region variables
+    public GameObject rightSideLeftDoor;
+    public GameObject rightSideRightDoor;
+    public GameObject leftSideLeftDoor;
+    public GameObject leftSideRightDoor;
+    #endregion
+
+    void Start ()
+    {
+        rightSideLeftDoor = transform.GetChild(0).gameObject;
+        rightSideRightDoor = transform.GetChild(2).gameObject;
+        leftSideLeftDoor  = transform.GetChild(1).gameObject;
+        leftSideRightDoor = transform.GetChild(3).gameObject;
+    }
+	
+	
+	void Update ()
+    {
+		
+	}
+
+  
+
+}
---
+++ VR Platform Edge/Assets/Train.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// This class is to control the behaviour of the train.
+/// </summary>
+public class Train : MonoBehaviour {
+
+    public List<Animator> doors = new List<Animator>();
+
+    public void Start()
+    {
+        doors = (transform.GetChild(0).transform.GetComponentsInChildren<Animator>().ToList());
+    }
+
+    /// <summary>
+    /// Opens all the train doors at the same time
+    /// </summary>
+    public void OpenTrainDoors()
+    {
+        Debug.Log("A");
+        for(int i = 0; i < doors.Count; i++)
+        {
+            doors[i].SetTrigger("TriggerOpenDoors");
+        }
+    }
+}
