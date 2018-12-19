d5426a2d5394afa6668320cb4d886b62740b4e06
julian_stopher <julianstopher@outlook.com>
2018-12-19 12:36:35 +0000

    Preparing repo for first Mirage build

---
+++ VR Platform Edge/Assets/Scripts/VideoSettings360.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoSettings360 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+
+        GetComponent<Camera>().stereoSeparation = 0.064f; // Eye separation (IPD) of 64mm.
+
+       
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
